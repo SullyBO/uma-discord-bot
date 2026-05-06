2cde1775a69c787802d28594ea0ff3a2ab58a1d5
SullyBO <193281232+SullyBO@users.noreply.github.com>
2026-05-06 12:10:53 +0300

    refactor: add logging to deploy-commands script

--- src/deploy-commands.ts
+++ src/deploy-commands.ts
@@ -1,22 +1,24 @@
 import { REST, Routes } from 'discord.js';
 import * as dotenvFlow from 'dotenv-flow';
 import { data as umaCommand } from './commands/uma';
+import { data as umasCommand } from './commands/umas';
 
 dotenvFlow.config();
 
 const token = process.env.DISCORD_TOKEN!;
 const clientId = process.env.CLIENT_ID!;
 
-const commands = [umaCommand.toJSON()];
+const commands = [umaCommand.toJSON(), umasCommand.toJSON()];
 
 const rest = new REST().setToken(token);
 
 (async () => {
   try {
-    console.log('Registering slash commands...');
+    console.log(`Registering ${commands.length} slash commands...`);
+    commands.forEach((c) => console.log(` - /${c.name}`));
     await rest.put(Routes.applicationCommands(clientId), { body: commands });
     console.log('Done.');
   } catch (error) {
-    console.error(error);
+    console.error('Failed to register commands:', error);
   }
 })();
